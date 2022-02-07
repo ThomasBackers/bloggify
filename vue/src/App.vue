4cbd14c883d5e7a91f18d0297ade0a677b91bee5
ThomasBackers <thomasbackers@gmail.com>
2022-02-07 11:51:09 +0100

    store works properly (and it's fire)

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,19 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+<script>
 import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  computed: {
+    ...mapState(['user'])
+  }
+}
 </script>
 
 <template>
+  <pre>{{ user.data.name }}</pre>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
