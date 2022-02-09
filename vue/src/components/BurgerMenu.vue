3d7ce02c37fc74e85599ce508464de218725dbca
ThomasBackers <thomasbackers@gmail.com>
2022-02-09 11:51:18 +0100

    burger menu 'onClick' opening animation done

--- vue/src/components/BurgerMenu.vue
+++ vue/src/components/BurgerMenu.vue
@@ -1,17 +1,30 @@
 <script setup>
+import { ref } from 'vue'
+
+const topBar = ref()
+const midBar = ref()
+const bottomBar = ref()
+
+const assignAnim = (eltRef, animName) => {
+  eltRef.style.animation = `${animName} 0.2s ease-out forwards`
+}
 </script>
 
 <template>
   <button
     class="burger-menu"
     type="button"
-    @click="() => {}"
+    @click="() => {
+      assignAnim(topBar, 'openingTopBar')
+      assignAnim(midBar, 'openingMidBar')
+      assignAnim(bottomBar, 'openingBottomBar')
+    }"
   >
-    <div class="burger-menu__bar"></div>
+    <div class="burger-menu__bar" ref="topBar"></div>
 
-    <div class="burger-menu__bar"></div>
+    <div class="burger-menu__bar" ref="midBar"></div>
 
-    <div class="burger-menu__bar"></div>
+    <div class="burger-menu__bar" ref="bottomBar"></div>
   </button>
 </template>
 
@@ -23,7 +36,6 @@
   flex-wrap: wrap;
 
   &__bar {
-    position: relative;
     background-color: $lightest-color;
     width: 100%;
     height: 2px;
@@ -34,19 +46,62 @@
 // OPENING ANIMATION
 @keyframes openingTopBar {
   0% {
-    transform: translateY(0);
+    transform: translateY(0) rotate(0);
+  }
+  50% {
+    transform: translateY(5px) rotate(0);
   }
   100% {
-    transform: translateY(3px);
+    transform: translateY(5px) rotate(45deg);
+  }
+}
+
+@keyframes openingMidBar {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
   }
 }
 
 @keyframes openingBottomBar {
   0% {
-    transform: translateY(0);
+    transform: translateY(0) rotate(0);
+  }
+  50% {
+    transform: translateY(-5px) rotate(0);
   }
   100% {
-    transform: translateY(-3px);
+    transform: translateY(-5px) rotate(-45deg);
+  }
+}
+
+// CLOSING ANIMATION
+@keyframes closingTopBar {
+  0% {
+    transform: translateY(5px) rotate(45deg);
+  }
+  50% {
+    transform: translateY(5px) rotate(0);
+  }
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+}
+
+@keyframes closingBottomBar {
+  0% {
+    transform: translateY(-5px) rotate(-45deg);
+  }
+  50% {
+    transform: translateY(-5px) rotate(0);
+  }
+  100% {
+    transform: translateY(0) rotate(0);
   }
 }
 </style>
