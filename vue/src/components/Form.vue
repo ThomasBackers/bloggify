25df76e2a7ad2f795169fafeff2e8f1ba2fc98cf
Thomas Backers <thomasbackers@gmail.com>
2022-02-17 11:03:28 +0100

    reusable Form component done

---
+++ vue/src/components/Form.vue
@@ -0,0 +1,53 @@
+<script setup>
+defineProps({
+  sectionName: String,
+  action: {
+    type: String,
+    default: ''
+  },
+  method: {
+    type: String,
+    default: 'post'
+  },
+  inputs: Array,
+  buttonLabel: String
+})
+</script>
+
+<template>
+  <form
+    :class="`${sectionName}__form`"
+    :action="action"
+    :method="method"
+  >
+    <label
+      v-for="(input, i) of inputs"
+      :key="i"
+      :class="`${sectionName}__form__label${input.type === 'checkbox' ? '-checkbox' : ''}`"
+    >
+      <input
+        v-if="input.type === 'checkbox'"
+        type="checkbox"
+        :class="`${sectionName}__form__label-checkbox__checkbox`"
+      >
+
+      {{ input.label }}
+
+      <input
+        v-if="input.type === 'text'"
+        type="text"
+        :class="`${sectionName}__form__label__input`"
+      >
+
+      <textarea
+        v-if="input.type === 'textarea'"
+        rows="10"
+        :class="`${sectionName}__form__label__input`"
+      ></textarea>
+    </label>
+
+    <button type="submit" :class="`${sectionName}__form__button`">
+      {{ buttonLabel }}
+    </button>
+  </form>
+</template>
--- vue/src/views/Contact.vue
+++ vue/src/views/Contact.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Form from '../components/Form.vue'
 </script>
 
 <template>
@@ -8,40 +9,32 @@
         contact
       </h2>
 
-      <form
-        class="contact__form"
-        action=""
-        method=""
-      >
-        <label class="contact__form__label">
-          your name (required)
-          <input type="text" class="contact__form__label__input">
-        </label>
-
-        <label class="contact__form__label">
-          your email (required)
-          <input type="text" class="contact__form__label__input">
-        </label>
-
-        <label class="contact__form__label">
-          subject
-          <input type="text" class="contact__form__label__input">
-        </label>
-
-        <label class="contact__form__label">
-          your message
-          <textarea class="contact__form__label__input" rows="10"></textarea>
-        </label>
-
-        <label class="contact__form__label-checkbox">
-          <input type="checkbox" class="contact__form__label-checkbox__checkbox">
-          I am happy to be contacted regarding my enquiry
-        </label>
-
-        <button class="contact__form__button" type="submit">
-          send
-        </button>
-      </form>
+      <Form
+        sectionName="contact"
+        :inputs="[
+          {
+            label: 'your name (required)',
+            type: 'text'
+          },
+          {
+            label: 'your email (required)',
+            type: 'text'
+          },
+          {
+            label: 'subject',
+            type: 'text'
+          },
+          {
+            label: 'your message',
+            type: 'textarea'
+          },
+          {
+            label: 'I am happy to be contacted regarding my enquiry',
+            type: 'checkbox'
+          }
+        ]"
+        buttonLabel="send"
+      />
     </section>
   </main>
 </template>
